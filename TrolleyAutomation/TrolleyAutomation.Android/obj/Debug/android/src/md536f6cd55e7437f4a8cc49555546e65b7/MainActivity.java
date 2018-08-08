322ce9f83377f5435c3e5d69d11daafb2cfe4ee4
lalit bokde <lalitthinker@gmail.com>
2018-08-08 22:19:21 +0530

    upload

---
+++ TrolleyAutomation/TrolleyAutomation.Android/obj/Debug/android/src/md536f6cd55e7437f4a8cc49555546e65b7/MainActivity.java
@@ -0,0 +1,47 @@
+package md536f6cd55e7437f4a8cc49555546e65b7;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("TrolleyAutomation.Droid.MainActivity, TrolleyAutomation.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("TrolleyAutomation.Droid.MainActivity, TrolleyAutomation.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ TrolleyAutomation/TrolleyAutomation/MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TrolleyAutomation
+{
+	public partial class MainPage : MasterDetailPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        //protected async override void OnAppearing()
+        //{
+          
+          
+        //}
+        //int i = 1;
+        //private bool CheckPositionAndUpdateSlider()
+        //{
+
+       
+        //        SLTrolley1.IsVisible = false;
+        //    SLTrolley3.IsVisible = false;
+        //    SLTrolley5.IsVisible = false;
+        //    SLTrolley7.IsVisible = false;
+
+        //    SLTrolley9.IsVisible = false;
+        //    SLTrolley11.IsVisible = false;
+        //    SLTrolley13.IsVisible = false;
+        //    SLTrolley15.IsVisible = false;
+        //    SLTrolley17.IsVisible = false;
+        //    SLTrolley19.IsVisible = false;
+        //    Trolley1(i);
+          
+        //    i++;
+        //    if (i == 8)
+        //    { i = 1; }
+        //      return true;
+        //}
+
+        //private void Trolley1(int i)
+        //{
+        //    switch (i)
+        //    {
+        //        case 1:
+        //            SLTrolley1.IsVisible = true;
+        //            break;
+        //        case 2:
+        //            SLTrolley3.IsVisible = true;
+        //            break;
+        //        case 3:
+        //            SLTrolley5.IsVisible = true;
+        //            break;
+
+        //        case 4:
+        //            SLTrolley7.IsVisible = true;
+        //            break;
+
+        //        case 5:
+        //            SLTrolley9.IsVisible = true;
+        //            break;
+
+        //        case 6:
+        //            SLTrolley11.IsVisible = true;
+        //            break;
+
+        //        case 7:
+        //            SLTrolley13.IsVisible = true;
+        //            break;
+
+        //        case 8:
+        //            SLTrolley15.IsVisible = true;
+        //            break;
+
+        //        default:
+        //            break;
+        //    }
+
+        //}
+
+        //private void Button_Clicked(object sender, EventArgs e)
+        //{
+        //      Device.StartTimer(TimeSpan.FromSeconds(5000), CheckPositionAndUpdateSlider);
+        //}
+    }
+}
